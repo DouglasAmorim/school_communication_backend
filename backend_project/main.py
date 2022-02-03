d58969bba2d56986c51613fbea71fd4fdbf75d20
DouglasAmorim <amorimdos@gmail.com>
2022-02-03 15:27:09 -0300

    updated db | base API implementation

--- backend_project/main.py
+++ backend_project/main.py
@@ -18,12 +18,38 @@ class Alunos(Resource):
 class AlunoById(Resource):
     def get(self, id):
         conn = db_connect.connect()
-        query = conn.execute("select * from user where id =%d" % int(id))
+        query = conn.execute("select * from Alunos where AlunosId =%d" % int(id))
         result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
         return jsonify(result)
 
+
+class ContactById(Resource):
+    def get(self, id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from Alunos where AlunosId = %d" % int(id))
+
+        result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+
+        turmaId = result[0]['TurmaId']
+        query = conn.execute("select * from Turma_has_Professores where TurmaId = %d" % int(turmaId))
+
+        result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+
+        professorId = result[0]['ProfessoresId']
+
+        print(professorId)
+
+        query = conn.execute("select * from Professores where ProfessoresId = %d" % int(professorId))
+        result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+
+        print(result)
+
+        return jsonify(result)
+
+
 api.add_resource(Alunos, '/alunos')
 api.add_resource(AlunoById, '/alunos/<id>')
+api.add_resource(ContactById, '/alunos/<id>/contacts')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
